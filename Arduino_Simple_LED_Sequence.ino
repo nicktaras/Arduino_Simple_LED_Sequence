419f3d4f216de02f6e14b9ccaacb78f2cf62ea2b
Nick Taras <nicktaras@hotmail.co.uk>
2019-06-22 22:39:35 +1000

    initial commit

---
+++ Arduino_Simple_LED_Sequence.ino
@@ -0,0 +1,43 @@
+
+int ledPins[] = {6,7,8,9,10,11,12,13};
+int ledPinLength = 7;
+int currArrayLed = 0;
+boolean isIncrementing = true;
+
+void setup() {               
+  for(int i = 0; i < 8; i ++){
+    pinMode(ledPins[i], OUTPUT);   
+  }  
+}
+
+void loop() {
+  linearSequence();
+}
+
+void currArrayHandler(){
+   if(currArrayLed == ledPinLength){
+      isIncrementing = false;
+   } 
+   else if(currArrayLed == 0) {
+     isIncrementing = true;
+   }
+   updateCurrLed();
+}
+
+void updateCurrLed(){
+  if(isIncrementing == true){
+    currArrayLed++; 
+  } else {
+    currArrayLed--;     
+  }
+}
+
+void linearSequence(){
+  digitalWrite(ledPins[currArrayLed], 100); 
+  delay(100);                     
+  digitalWrite(ledPins[currArrayLed], LOW);   
+  delay(100); 
+  currArrayHandler();
+}
+
+
